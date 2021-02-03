a10cccf673040be8d08f0a4b43b9a4eed4c1e70c
Klaudia Stewart <klaudiastewart@gmail.com>
2021-02-03 14:40:55 -0700

    finish iteration 3, add iteration 4

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
   attr_reader :opening_time,
-              :name
+              :name,
+              :dishes,
+              :lunch
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name         = name
+    @dishes       = []
+    @lunch        = true
+  end
+
+  def closing_time(number)
+     close = number + @opening_time.to_i
+     return "#{close}:00"
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def open_for_lunch?
+    if @lunch == true
+      @opening_time <= '12:00'
+    else
+      lunch? == false
+    end
+  end
+
+  def menu_dish_names
+    menu_dish_names = []
+    @dishes.each do |dish|
+      menu_dish_names << dish.upcase
+    end
+    menu_dish_names
+  end
+
+  def announce_closing_time(number)
+    announcement = number + @opening_time.to_i
+    return "Il Posto will be closing at #{announcement}:00AM"
   end
 end
